71065e8ab5ae11ead81e2f43113e4b48f1dd7c73
Akshay Mhatre <akshay1188@gmail.com>
2014-11-19 13:33:30 -0800

    added sign up

--- routes/mysql.js
+++ routes/mysql.js
@@ -3,6 +3,43 @@
  * 
  */
 
-
 var mysql = require('mysql');
+
+var pool  = mysql.createPool({
+	host     : 'localhost',
+	user     : 'root',
+	password : '',
+	port: '3306',
+	database: 'Ebay'
+});
+
+exports.signup = function (req, res, callback){
+	var signupData = {
+		email:req.body.email,
+		password:req.body.password,
+		firstName:req.body.firstname,
+		lastName:req.body.lastname,
+		addressMain:req.body.address,
+		city:req.body.city,
+		state:req.body.state,
+		zip:req.body.zip,
+		isSeller:req.body.isSeller
+	};
+	
+	pool.getConnection(function(err, connection) {
+		if(err){
+			console.log('error');
+			console.log(err);
+		}
+		connection.query('INSERT INTO User SET	?', signupData,
+				function(err, result) {
+			connection.release();
+			if(err) {
+				callback(err);
+				return;
+			}
+			callback(false, result);
+		});
+	});
+};
 

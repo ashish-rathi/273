4ee6f982165b9b32cc82406a3cc04c21e25e114c
devenugoogly <deven_googly@yahoo.com>
2014-11-27 04:25:53 -0500

    updating changes

--- routes/categories.js
+++ routes/categories.js
@@ -1,3 +1,17 @@
 /**
  * New node file
  */
+var ejs = require("ejs");
+
+exports.demo = function(req,res)
+{
+	ejs.renderFile('./views/signin.ejs',{error:'',message:''},function(err, result){
+		  if (!err) {
+	          res.end(result);
+	      }
+	      else {
+	          res.end('An error occurred');
+	          console.log(err);
+	      }
+	  });	
+}

572887139f5c16107a277f5ad9d25ed854e47b96
devenugoogly <deven_googly@yahoo.com>
2014-12-02 09:38:03 -0500

    updating function returning categry name

--- routes/categories.js
+++ routes/categories.js
@@ -7,41 +7,50 @@ exports.product = function(req,res)
 	var catId = req.params.catId;
 	var prodId = req.params.prodId;
 	console.log(catId + " " + prodId);
-	ejs.renderFile('./views/product.ejs',{session:req.session},function(err, result){
-		  if (!err) {
+	var categoryType = getCategoryType(catId);
+	customMysql.get_product(prodId, catId, function(err, products) {
+		console.log("product length " +products.length);
+		console.log(products);
+		if(products.length > 0){
+			var jsonString = JSON.stringify(products);
+			var productCatalogs = JSON.parse(jsonString);
+			ejs.renderFile('./views/product.ejs',{session:req.session,category:categoryType,productCatalog:productCatalogs},function(err, result){
+			if (!err) {
 	          res.end(result);
-	      }
-	      else {
+			}
+			else {
 	          res.end('An error occurred');
 	          console.log(err);
-	      }
-	  });	
+			}
+			});
+		}
+	});	
 }
 
 
 exports.category = function(req,res)
 {
 	var id = req.params.id;
-	var categoryType;
-	if(id == 324)
-		categoryType = 'Mobiles / Tablets';
-	else if(id == 325)
-		categoryType = 'Computer / Laptops';
-	else if(id == 326)
-		categoryType = 'TV';
-	else if(id == 327)
-		categoryType = 'Cameras';
-	else if(id == 328)
-		categoryType = 'Cds / Dvds';
-	else if(id == 329)
-		categoryType = 'Accessories';
-	else
-		categoryType = 'Others';
+	var categoryType = getCategoryType(id);
+//	if(id == 324)
+//		categoryType = 'Mobiles / Tablets';
+//	else if(id == 325)
+//		categoryType = 'Computer / Laptops';
+//	else if(id == 326)
+//		categoryType = 'TV';
+//	else if(id == 327)
+//		categoryType = 'Cameras';
+//	else if(id == 328)
+//		categoryType = 'Cds / Dvds';
+//	else if(id == 329)
+//		categoryType = 'Accessories';
+//	else
+//		categoryType = 'Others';
 	
 	console.log("id is "+id);
 	customMysql.get_products_for_category(id, function(err, products) {
 		console.log("length " +products.length);
-		if(products.length >= 0){
+		if(products.length > 0){
 			var jsonString = JSON.stringify(products);
 			var productCatalogs = JSON.parse(jsonString);
 			ejs.renderFile('./views/categories.ejs',{session:req.session,category:categoryType,productCatalog:productCatalogs},function(err, result){
@@ -60,12 +69,13 @@ exports.category = function(req,res)
 
 //get the products searched by user
 exports.search = function(req,res){
-	var queryData = url.parse(req.url, true).query;
+	var queryData = url.parse(req.url, true).query;	
 	customMysql.search_products(queryData._nkw,function(err,products){
 		if(products.length > 0){
+			var result = true;
 			var jsonString = JSON.stringify(products);
 			var productCatalogs = JSON.parse(jsonString);
-			ejs.renderFile('./views/search.ejs',{session:req.session,productCatalog:productCatalogs,searchString:queryData._nkw},function(err, result){
+			ejs.renderFile('./views/search.ejs',{session:req.session,productCatalog:productCatalogs,searchString:queryData._nkw,result:result},function(err, result){
 			if (!err) {
 	          res.end(result);
 			}
@@ -75,5 +85,43 @@ exports.search = function(req,res){
 			}
 			});
 		}
+		else{
+				var result = false;
+				var jsonString = JSON.stringify(products);
+				var productCatalogs = JSON.parse(jsonString);
+				ejs.renderFile('./views/search.ejs',{session:req.session,productCatalog:productCatalogs,searchString:queryData._nkw,result:result},function(err, result){
+				if (!err) {
+		          res.end(result);
+				}
+				else {
+		          res.end('An error occurred');
+		          console.log(err);
+				}
+				});
+		}
 	});
+}
+
+
+function getCategoryType(id)
+{
+	
+	var categoryType;
+	if(id == 324)
+		categoryType = 'Mobiles / Tablets';
+	else if(id == 325)
+		categoryType = 'Computer / Laptops';
+	else if(id == 326)
+		categoryType = 'TV';
+	else if(id == 327)
+		categoryType = 'Cameras';
+	else if(id == 328)
+		categoryType = 'Cds / Dvds';
+	else if(id == 329)
+		categoryType = 'Accessories';
+	else
+		categoryType = 'Others';
+	
+	return categoryType;
+
 }
